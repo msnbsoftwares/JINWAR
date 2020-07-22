89e7ae73224fc1ef03ef5892a424e0e4ac5f22c8
msnbsoftwares <msnbsoftwares@gmail.com>
2020-07-22 22:41:53 +0530

    [ADDED] : Donate Fragment and ViewModel

--- app/src/main/java/com/softgen/jinwar/ui/donate/DonateFragment.java
+++ app/src/main/java/com/softgen/jinwar/ui/donate/DonateFragment.java
@@ -1,4 +1,4 @@
-package com.softgen.jinwar.ui.gallery;
+package com.softgen.jinwar.ui.donate;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.softgen.jinwar.R;
 
-public class GalleryFragment extends Fragment {
+public class DonateFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private DonateViewModel donateViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        donateViewModel =
+                ViewModelProviders.of(this).get(DonateViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_donate, container, false);
+        final TextView textView = root.findViewById(R.id.donationText);
+        donateViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/softgen/jinwar/ui/donate/DonateViewModel.java
+++ app/src/main/java/com/softgen/jinwar/ui/donate/DonateViewModel.java
@@ -1,16 +1,16 @@
-package com.softgen.jinwar.ui.gallery;
+package com.softgen.jinwar.ui.donate;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class DonateViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public DonateViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is Donation fragment");
     }
 
     public LiveData<String> getText() {
