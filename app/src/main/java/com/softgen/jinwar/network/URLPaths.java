a0b7b50ea4fece44445c56e79080a4d36d0b4065
msnbsoftwares <msnbsoftwares@gmail.com>
2020-07-22 22:10:13 +0530

    [ADDED] : Adding Dharmashala API Endpoint to URLPaths

--- app/src/main/java/com/softgen/jinwar/network/URLPaths.java
+++ app/src/main/java/com/softgen/jinwar/network/URLPaths.java
@@ -2,7 +2,8 @@ package com.softgen.jinwar.network;
 
 public enum URLPaths {
     USERAUTHENTICATION("/userauthenticate"),
-    CHECKAPPVERSION("/checkAppVersion");
+    CHECKAPPVERSION("/checkAppVersion"),
+    AVAILABLE_DHARMASHALAS("/getAvailableDharmashalas");
 
     private final String urlPath;
     URLPaths(String urlPath){this.urlPath = urlPath;}
